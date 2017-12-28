04d6736c78665f35e9c65c18d252a8eb109eeece
Nitin Gaba <ngaba@sapient.com>
2017-12-28 17:48:49 +0530

    add new filey

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'homeComponent',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+// Import router directives
+// Deprecated
+// import { ROUTER_DIRECTIVES } from '@angular/router';
+// App Component class
+export class homeComponent {}
